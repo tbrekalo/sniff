fda6db301323c9b62e9fddf62c87075bbe40054c
tbrekalo <t@tbrekalo.xyz>
2023-04-25 08:47:39 +0000

    tbrekalo: version and formatting

--- src/main.cc
+++ src/main.cc
@@ -4,29 +4,49 @@
 
 #include "biosoup/nucleic_acid.hpp"
 #include "cxxopts.hpp"
+#include "version.h"
 
 std::atomic<std::uint32_t> biosoup::NucleicAcid::num_objects = 0;
 
 int main(int argc, char** argv) {
-  auto options =
-      cxxopts::Options("sniff", "pair up potential reverse complement reads");
-  /* clang-format off */
-  options.add_options()
-    ("h,help", "display help")
-    ("t,threads", "number of threads available",
-      cxxopts::value<std::uint32_t>()->default_value("1"))
-    ("input", "input fasta/fastq file", cxxopts::value<std::string>());
-  /* clang-format on */
+  try {
+    auto options =
+        cxxopts::Options("sniff", "pair up potential reverse complement reads");
+    /* clang-format off */
+    options.add_options()
+      ("h,help", "print help")
+      ("v,version", "print version")
+      ("t,threads", "number of threads to use",
+        cxxopts::value<std::uint32_t>()->default_value("1"))
+      ("input", "input fasta/fastq file", cxxopts::value<std::string>());
+    /* clang-format on */
 
-  options.parse_positional({"input-file"});
-  auto result = options.parse(argc, argv);
+    options.parse_positional({"input-file"});
+    auto result = options.parse(argc, argv);
 
-  if (result.count("help")) {
-    std::cout << options.help() << std::endl;
-    return EXIT_SUCCESS;
+    auto early_quit = false;
+    if (result.count("version")) {
+      /* clang-format off */
+      std::cout << sniff_VERSION_MAJOR << '.'
+                << sniff_VERSION_MINOR << '.'
+                << sniff_VERSION_PATCH << std::endl;
+      /* clang-format on */
+      early_quit = true;
+    }
+
+    if (result.count("help")) {
+      std::cout << options.help() << std::endl;
+      early_quit = true;
+    }
+
+    if (early_quit) {
+      return EXIT_SUCCESS;
+    }
+
+    auto const n_threads = result["threads"].as<std::uint32_t>();
+  } catch (std::exception const& e) {
+    std::cerr << e.what() << std::endl;
   }
-
-  auto const n_threads = result["threads"].as<std::uint32_t>();
 
   return EXIT_SUCCESS;
 }
