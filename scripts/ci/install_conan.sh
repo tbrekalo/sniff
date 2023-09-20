55bf99524f6a05e545773e3f0d7268d7b0763742
tbrekalo <t@tbrekalo.xyz>
2023-09-20 21:03:00 +0200

    build: adding setuptools to conan install script

--- scripts/ci/install_conan.sh
+++ scripts/ci/install_conan.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eou pipefail
 
-apt-get update && apt-get install \
-  -y --no-install-recommends git ninja-build pkg-config python3-pip
+apt-get update && apt-get install -y --no-install-recommends \
+  git ninja-build pkg-config python3-pip python3-setuptools
 pip3 install conan==2.0
 
 conan profile detect -f
